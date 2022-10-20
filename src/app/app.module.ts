12bf91d5c99dc132f2a80baccd42adb2070ce73c
André Lagacione <lagacioneandre@gmail.com>
2022-10-20 16:54:31 -0300

    reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ScoreboardPageComponent } from './scoreboard-page/scoreboard-page.component';
+import { StoreModule } from '@ngrx/store';
+import { scoreboardReducer } from './scoreboard-page/reducers/scoreboard.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ScoreboardPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ game: scoreboardReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
